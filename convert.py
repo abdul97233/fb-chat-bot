3d4e07fa4c0456733ec10f044047fa878004be4d
Md Abdul Kalam Shekh <37735290+abdul97233@users.noreply.github.com>
2022-04-02 09:52:51 +0545

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,17 @@
+# Author: Abdul
+# Telegram: t.me/abdul97233
+
+import json
+
+cookies = input("Paste fb cookies here: ")
+cookies = cookies.replace("=", ":").replace(";", ",")[:-1]
+cookies_dict = {}
+cookies = cookies.split(",")
+
+for values in cookies:
+    values = values.split(":")
+    cookies_dict[values[0]] = values[1]
+
+json_cookies = json.dumps(cookies_dict)
+
+print(f'\n------------------->> JSON COOKIES <<---------------------\n\n{json_cookies}\n')
