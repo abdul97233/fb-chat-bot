dbfda83255f7a2d34a4f13cb2fc355ca89b5cc7f
Md. Abdul Kalam Shekh <37735290+abdul97233@users.noreply.github.com>
2022-07-29 11:52:29 +0545

    issue fixed

--- fb.py
+++ fb.py
@@ -625,9 +625,9 @@ class ChatBot(Client):
 cookies = {
     "sb": "xasyYmAoy1tRpMGYvLxgkHBF",
     "fr": "0NxayJuewRHQ30OX3.AWVJwIYNh0Tt8AJv6kSwDamhkoM.BiMrVd.Iu.AAA.0.0.BiMtVZ.AWXMVaiHrpQ",
-    "c_user": "",
+    "c_user": f"{os.environ.get('c_user')}",
     "datr": "xasyYs51GC0Lq5H5lvXTl5zA",
-    "xs": ""
+    "xs": f"{os.environ.get('xs_value')}" 
 }
 
 
